e430e35bb20e8e364002b7569830fef535ee1d72
patelrakesh <pateanil82@gmail.com>
2021-12-23 10:42:48 +0530

    88-templating

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-const http = require('http');
+// const http = require('http');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.set("view engine", "pug");
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", adminRoutes.routes);
+app.use(shopRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  // res.status(404).send('<h1>Page noy found</h1>');
+  // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+  res.status(404).render("404", { docTitle: "Page not found" });
+});
+
+// app.use('/',(req, res, next)=>{
+//     console.log("this always run");
+//     next();
+// });
+
+// app.use('/add-product',(req, res, next)=>{
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// })
+
+// app.post('/product',(req, res, next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// })
+
+// app.use('/',(req, res, next)=>{
+//     console.log("In the second middleware!!")
+//     res.send('<h1>HTML from express!!</h1>');
+// })
+app.listen(3000);
+// const server = http.createServer(app);
+
+// server.listen(3000);
